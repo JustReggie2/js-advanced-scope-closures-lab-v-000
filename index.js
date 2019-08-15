fc4ef69678f7aad30f0663e60f9479c5b2e704d6
github username <github email address>
2019-08-14 21:18:07 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(blockRange) {
+  return function(rg1, rg2) {
+    let range = parseInt(rg1) - parseInt(rg2)
+    if (Math.abs(range) > blockRange) {
+      let outOfRange = Math.abs(range) - blockRange
+      return `${outOfRange} blocks out of range`;
+    } else {
+      return `within range by ${Math.abs(range)}`;
+    }
+  }
+}
+
+function produceTipCalculator(price) {
+  return function(tipPercent) {
+    return tipPercent * price;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  return class {
+    constructor(name) {
+      this.id = ++driverId;
+      this.name = name;
+    }
+  };
+}
